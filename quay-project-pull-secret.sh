774af460772bc242a6b77d22877db2f3741bbcf6
Andrew Pitt <apitt@redhat.com>
2020-01-06 16:31:16 -0500

    Fixed dc and script.

--- quay-project-pull-secret.sh
+++ quay-project-pull-secret.sh
@@ -13,8 +13,8 @@ devproject="$projectprefix-dev"
 uatproject="$projectprefix-uat"
 
 echo "Creating quay pull secret in $devproject and $uatproject projects"
-oc create secret docker-registry quay-cicd-secret --docker-server=quay.io --docker-username="$quayuser" --docker-password="$quaypass" --docker-email="$quayemail" -n "$devproject"
-oc create secret docker-registry quay-cicd-secret --docker-server=quay.io --docker-username="$quayuserr" --docker-password="$quaypass" --docker-email="$quayemail" -n "$uatproject"
+oc create secret docker-registry quay-cicd-secret --docker-server=quay.io --docker-username="$quayuser" --docker-password="$quaypass" --docker-email=$quayemail -n "$devproject"
+oc create secret docker-registry quay-cicd-secret --docker-server=quay.io --docker-username="$quayuser" --docker-password="$quaypass" --docker-email=$quayemail -n "$uatproject"
 
 oc secrets link default quay-cicd-secret --for=pull -n "$devproject"
 oc secrets link default quay-cicd-secret --for=pull -n "$uatproject"
