c75c05b50aaf16e76f3f956b918bdcd090330517
Andrew Pitt <apitt@redhat.com>
2020-01-25 16:10:21 -0500

    Updated new-env.

--- new-env.sh
+++ new-env.sh
@@ -3,6 +3,12 @@
 read -p 'Base apps url (e.g. app.ocp.pitt.ca): ' APPS_BASE_URL
 read -p 'Git repository url: ' GIT_URL
 read -p 'Git branch (e.g. master): ' GIT_REF
+read -p 'Quay read/write username: ' quayrwuser
+read -p 'Quay reqd/write email: ' quayrwemail
+read -sp 'Quay read/write password: ' quayrwpass
+read -p 'Quay read-only username: ' quayrouser
+read -p 'Quay read-only email: ' quayroemail
+read -sp 'Quay read-only password: ' quayropass
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     FIND_ROUTE_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 sed -i \'\' \'s/apps\\.example\\.com/'
@@ -36,5 +42,11 @@ echo ""
 eval $FIND_AND_REPLACE_ROUTE
 eval $FIND_AND_REPLACE_REPO
 eval $FIND_AND_REPLACE_BRANCH
+echo ""
+
+echo "Creting Sealed Secrets."
+kubeseal --cert ~/kubeseal.pem <blah.json >/gitops/cicd/builds/quay-creds-sealed-secret.json
+kubeseal --cert ~/kubeseal.pem <blah2.json >/gitops/java/petclinic/overlays/dev/quay-readonly-sealed-secret.json
+kubeseal --cert ~/kubeseal.pem <blah3.json >/gitops/java/petclinic/overlays/dev/quay-readonly-sealed-secret.json
 
 echo "Done!"
--- update-rhpds-route.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find $PWD \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '' 's/'"$1"'/'"$2"'/g'
-
