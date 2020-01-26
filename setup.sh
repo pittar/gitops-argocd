ecc72de1f728aa1c0db207afb14cabafeef8e85f
Andrew Pitt <apitt@redhat.com>
2020-01-25 23:50:13 -0500

    LANG

--- setup.sh
+++ setup.sh
@@ -33,9 +33,9 @@ else
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    FIND_ROUTE_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 sed -i \'\' \'s/apps\\.example\\.com/'
-    FIND_REPO_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 sed -i \'\' \'s/git\\.url\\.git/'
-    FIND_BRANCH_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 sed -i \'\' \'s/targetRevision:\ master/targetRevision:\ '
+    FIND_ROUTE_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 LANG=C sed -i \'\' \'s/apps\\.example\\.com/'
+    FIND_REPO_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 LANG=C sed -i \'\' \'s/git\\.url\\.git/'
+    FIND_BRANCH_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 LANG=C sed -i \'\' \'s/targetRevision:\ master/targetRevision:\ '
 else
     FIND_ROUTE_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 sed -i \'s/apps\\.example\\.com/'
     FIND_REPO_PREFIX=$'find $PWD \\( -type d -name .git -prune \\) -o -type f -print0 | xargs -0 sed -i \'s/git\\.url\\.git/'
@@ -49,7 +49,7 @@ REPO=$(sed 's/\./\\./g' <<< $GIT_URL)
 REPO=$(sed 's/\//\\\//g' <<< $REPO)
 BRANCH=$(sed 's/\./\\./g' <<< $GIT_REF)
 
-FIND_AND_REPLACE_ROUTE=$FIND_ROUTE_PREFIX$ROUTE$FIND_SUFFIX
+FIND_AND_REPLACE_ROUTE="$FIND_ROUTE_PREFIX$ROUTE$FIND_SUFFIX"
 FIND_AND_REPLACE_REPO="$FIND_REPO_PREFIX$REPO$FIND_SUFFIX"
 FIND_AND_REPLACE_BRANCH="$FIND_BRANCH_PREFIX$BRANCH$FIND_SUFFIX"
 
